0d015965d9f60cd6e28df72891772d6029739661
laxmishinde29 <laxmishinde2975@gmail.com>
2026-07-18 17:37:22 +0530

    Add files via upload

---
+++ Projects/Bootstrap/app/marvellous/marvellous.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marvellous',
+  imports: [],
+  templateUrl: './marvellous.component.html',
+  styleUrl: './marvellous.component.css'
+})
+export class MarvellousComponent {
+
+}
